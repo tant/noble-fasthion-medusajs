dd183045b166042d26a925f12b5138ce6b9759ce
Tan Tran <2188362+tant@users.noreply.github.com>
2025-06-05 11:29:03 +0700

    add supabase

--- medusa-config.ts
+++ medusa-config.ts
@@ -16,10 +16,7 @@ module.exports = defineConfig({
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     },
-    workerMode: process.env.MEDUSA_WORKER_MODE as
-      | "shared"
-      | "worker"
-      | "server",
+    workerMode: process.env.MEDUSA_WORKER_MODE as "shared" | "worker" | "server",
   },
   modules: [
     {
@@ -42,6 +39,29 @@ module.exports = defineConfig({
         },
       },
     },
+    {
+      resolve: "@medusajs/medusa/file",
+      options: {
+        providers: [
+          {
+            resolve: "@medusajs/medusa/file-s3",
+            id: "s3",
+            options: {
+              file_url: process.env.S3_FILE_URL,
+              access_key_id: process.env.S3_ACCESS_KEY_ID,
+              secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
+              region: process.env.S3_REGION,
+              bucket: process.env.S3_BUCKET,
+              endpoint: process.env.S3_ENDPOINT,
+              options: {
+                additional_client_config: {
+                  forcePathStyle: true,
+                },
+              },
+            },
+          },
+        ],
+      },
+    },
   ],
-
 });
